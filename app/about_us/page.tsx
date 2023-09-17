4f46cc03c5c61d6b2e00c9b27483547f40afb2b5
Zombie <adiya@ureca.com>
2023-09-17 23:06:40 +0800

    asd

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -9,11 +9,11 @@ export default function AboutUS() {
 			<div className="flex flex-col md:flex-row w-full items-center justify-between gap-10">
 				<div className="flex flex-col gap-10 w-full md:w-1/3">
 					<h2 className="text-black-900 text-xl md:text-[54px] font-semibold" >What We Do</h2>
-					<p className="text-black-900 text-sm md:text-[22px]">It is a long established fact that a reader will be distracted by the of readable content of a page 
+					<p className="text-black-900 md:leading-[150%] text-sm md:text-[22px]">It is a long established fact that a reader will be distracted by the of readable content of a page 
 					when lookings at its layouts the points of using 
 					that it has a more-or-less normal.</p>
 					<div>
-						<PButton className="flex gap-4 rounded-3xl py-[20px] text-white bg-black-900 px-[20px]">Our Concept <ArrowRight/></PButton>
+						{/* <PButton className="flex gap-4 rounded-3xl py-[20px] text-white bg-black-900 px-[20px]">Our Concept <ArrowRight/></PButton> */}
 					</div>
 				</div>
 				<div className="w-full md:w-1/2 rounded-[62px] overflow-hidden relative h-[300px] md:h-[500px]">
@@ -26,16 +26,16 @@ export default function AboutUS() {
 				</div>
 				<div className="flex flex-col gap-10 w-full md:w-1/3">
 					<h2 className="text-black-900 text-xl md:text-[54px] font-semibold" >The End Result</h2>
-					<p className="text-black-900 text-sm md:text-[22px]">It is a long established fact that a reader will be distracted by the of readable content of a page 
+					<p className="text-black-900  md:leading-[150%] text-sm md:text-[22px]">It is a long established fact that a reader will be distracted by the of readable content of a page 
 					when lookings at its layouts the points of using 
 					that it has a more-or-less normal.</p>
 					<div>
-						<PButton className="flex gap-4 rounded-3xl py-[20px] text-white bg-black-900 px-[20px]">Our Portfolio <ArrowRight/></PButton>
+						{/* <PButton className="flex gap-4 rounded-3xl py-[20px] text-white bg-black-900 px-[20px]">Our Portfolio <ArrowRight/></PButton> */}
 					</div>
 				</div>
 			</div>
 			<div className="w-full h-[300px] flex justify-center items-center relative border-primary-900 border-[10px] rounded-[60px] ">
-				<div className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-center leading-[40px] md:text-[36px] flex justify-center items-center bg-white h-[400px] max-w-[751px] px-4">
+				<div className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-center leading-[150%] md:text-[36px] flex justify-center items-center bg-white h-[400px] max-w-[751px] px-4">
 					Lever is dedicated to championing six transformative objectives aligned with the United Nations Sustainable Development Goals (SDGs), driving positive change and global impact. 
 				</div>
 			</div>
--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,18 @@ import { SMockPool, SOurService } from "@/sections";
 export default function Home() {
 	return (
 		<div className="flex px-2 gap-10 md:gap-[200px] mb-[96px] flex-col items-center justify-between ">
-			<div className="h-[calx(screen - 200px)] relative flex flex-col md:flex-row w-full container m-auto justify-between">
+			<div className=" relative h-[calx(screen - 200px)] relative flex flex-col md:flex-row w-full container m-auto justify-between">
 				<div className="flex flex-col gap-10">
 					<div className="p-10 font-semibold text-2xl md:text-[64px] W-FULL md:p-[90px] flex flex-col gap-4 md:gap-10">
 						<span>TAKE YOUR<br/></span>
 						<span><span className="text-[#276EF1]">ACADEMY</span> TO <br/></span>
 						<span>THE <span className="text-[#276EF1]">NEXT LEVEL</span> <br/></span>
 					</div>
+					<Image className="top-0 absolute" src="/group.png" alt="boy" width={417} height={600} />
 					<div className="flex pl-10 md:pl-[90px]  ">
 						<PButton className="bg-primary-900 text-white w-auto">Get Started</PButton>
+					</div>
+					<div className="absolute top-1 -left-4 bg-primary-900 rounded-full w-[30px] h-[30px] ">
 					</div>
 				</div>
 				<div className="relative">
@@ -24,6 +27,7 @@ export default function Home() {
 						<Image src="/boy.png" alt="boy" width={417} height={600} />
 
 					</div>
+					<Image className="-bottom-[50%] -right-10 absolute" src="/group.png" alt="boy" width={417} height={600} />
 					<div className="absolute -top-4 -left-4 border border-primary-900 rounded-full w-[538px] h-[538px] ">
 					</div>
 					<div className="absolute bottom-1 -left-4 bg-primary-900 rounded-full w-[53px] h-[53px] ">
